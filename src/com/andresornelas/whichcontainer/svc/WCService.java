4aa5dce01ac3e08292e7d72a43e185bc9a548723
Andres Ornelas <andres@twitter.com>
2013-07-12 19:51:59 -0700

    Prevent duplicate pans from being added

--- src/com/andresornelas/whichcontainer/svc/WCService.java
+++ src/com/andresornelas/whichcontainer/svc/WCService.java
@@ -3,6 +3,7 @@ package com.andresornelas.whichcontainer.svc;
 import android.app.IntentService;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.util.Log;
 
 import com.andresornelas.whichcontainer.WCContract;
@@ -32,10 +33,24 @@ public class WCService extends IntentService {
 
   private void addPan(Intent i) {
     Log.d(LOG_TAG, "Adding pan");
+    double capacity = i.getDoubleExtra(WCContract.Pans.Columns.CAPACITY, 0);
+    String unit = i.getStringExtra(WCContract.Pans.Columns.UNIT);
+    String brand = i.getStringExtra(WCContract.Pans.Columns.BRAND);
+
+    Cursor cursor = getContentResolver().query(WCContract.Pans.URI,
+            new String[] { WCContract.Pans.Columns.ID },
+            "CAPACITY = " + capacity + " AND UNIT = '" + unit + "' AND BRAND = '" + brand + "'",
+            null, null);
+
+    if (cursor.getCount() != 0) {
+      Log.d(LOG_TAG, "Pan already in the database!");
+      return;
+    }
+
     ContentValues pan = new ContentValues();
-    pan.put(WCContract.Pans.Columns.BRAND, i.getStringExtra(WCContract.Pans.Columns.BRAND));
-    pan.put(WCContract.Pans.Columns.CAPACITY, i.getDoubleExtra(WCContract.Pans.Columns.CAPACITY, 0));
-    pan.put(WCContract.Pans.Columns.UNIT, i.getStringExtra(WCContract.Pans.Columns.UNIT));
+    pan.put(WCContract.Pans.Columns.BRAND, brand);
+    pan.put(WCContract.Pans.Columns.CAPACITY, capacity);
+    pan.put(WCContract.Pans.Columns.UNIT, unit);
     pan.put(WCContract.Pans.Columns.AMOUNT, i.getIntExtra(WCContract.Pans.Columns.AMOUNT, 0));
     pan.put(WCContract.Pans.Columns.IS_CONTAINTER,
             i.getIntExtra(WCContract.Pans.Columns.IS_CONTAINTER, 0));
